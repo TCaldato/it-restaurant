6221555b940b5108950cf3d38b259cd312eec760
TCaldato <>
2024-01-07 12:35:32 +0000

    Connected Heroku to the PostgreSQL database

--- it_project/settings.py
+++ it_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hhhpzau*p519i_xrx383_7-zotc^!)*wum*)yls&sa%3p-ez7u"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
