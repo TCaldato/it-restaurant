765b92b5f133e6705c8cd9b76bff3cc72b6a256f
TCaldato <>
2024-01-16 12:49:33 +0000

    Styling messages

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,72 +1,79 @@
- /* jshint esversion: 8, jquery: true */
-
+/* jshint esversion: 8, jquery: true */
 
 /**
- * datepicker widget
- * long date format
- * limit date range from today + 12 months
- * disable weekends
+ * Initialize datepicker widget with specific settings
  */
+ function initializeDatepicker() {
+    const datepicker = $("#datepicker");
+    
+    datepicker.datepicker({
+        dateFormat: "MM d, yy",
+        minDate: 0,
+        maxDate: "+12M +0D",
+        beforeShowDay: $.datepicker.noWeekends
+    });
+
+    datepicker.datepicker("option", "showAnim", "fold");
+    datepicker.datepicker("setDate", '0');
+}
+
+/**
+ * Filter booking table by datepicker value
+ */
+function filterResults() {
+    const resultRows = document.querySelectorAll(".booking-row");
+    const filterData = $("#datepicker").val();
+
+    resultRows.forEach(row => {
+        row.classList.add("d-none");
+        if (row.innerText.includes(filterData)) {
+            row.classList.remove("d-none");
+        }
+    });
+}
+
+/**
+ * Filter view-booking-table by current user
+ */
+function myBookingsFilter() {
+    const resultRows = document.querySelectorAll(".booking-row");
+    const filterUser = $("#current-user").text();
+
+    resultRows.forEach(row => {
+        row.classList.add("d-none");
+        if (row.innerText.includes(filterUser)) {
+            row.classList.remove("d-none");
+        }
+    });
+}
+
+/**
+ * Unfilter booking table by removing "d-none" class
+ */
+function removeFilter() {
+    const resultRows = document.querySelectorAll(".booking-row");
+
+    resultRows.forEach(row => {
+        row.classList.remove("d-none");
+    });
+}
+
+/**
+ * Handle button click to update selected value
+ */
+function handleButtonClick() {
+    const val = $(this).val();
+    $('.btn').removeClass('selected');
+    $(this).addClass('selected');
+    $('.selectedVal').val(val);
+}
+
+// Document ready function
 $(document).ready(function () {
-   $("#datepicker").datepicker({
-      dateFormat: "MM d, yy",
-      minDate: +0,
-      maxDate: "+12M +0D",
-      beforeShowDay: $.datepicker.noWeekends
-   });
-   $("#datepicker").datepicker("option", "showAnim", "fold");
-   $("#datepicker").datepicker("setDate", '0');
-});
+    initializeDatepicker();
 
-
-// filter booking table by datepicker value
-const datepicker = document.getElementById("datepicker");
-const filterResults = (e) => {
-   let resultRows = document.querySelectorAll(".booking-row");
-   let filterData = datepicker.value;
-   resultRows.forEach(row => {
-      row.classList.add("d-none");
-      if (row.innerText.includes(filterData)) {
-         row.classList.remove("d-none");
-      }
-   });
-};
-
-$(datepicker).on("change", filterResults);
-
-
-// filter view-booking-table by current user
-const currentUser = document.getElementById("current-user");
-const myBookingsBtn = document.getElementById("table-filter-user");
-const myBookingsFilter = (e) => {
-   let resultRows = document.querySelectorAll(".booking-row");
-   let filterUser = currentUser.innerText;
-   resultRows.forEach(row => {
-      row.classList.add("d-none");
-      if (row.innerText.includes(filterUser)) {
-         row.classList.remove("d-none");
-      }
-   });
-};
-
-$(myBookingsBtn).on("click", myBookingsFilter);
-
-
-// unfilter booking table by removing "d-none" class
-const refreshBtn = document.getElementById("refresh-table");
-const removeFilter = (e) => {
-   let resultRows = document.querySelectorAll(".booking-row");
-   resultRows.forEach(row => {
-      row.classList.remove("d-none");
-   });
-};
-
-$(refreshBtn).on("click", removeFilter);
-$(".btn").click(function () {
-
-   var val = $(this).val();
-   $('.btn').removeClass('selected');
-   $(this).addClass('selected');
-   $('.selectedVal').val(val);
-
+    $("#datepicker").on("change", filterResults);
+    $("#table-filter-user").on("click", myBookingsFilter);
+    $("#refresh-table").on("click", removeFilter);
+    $(".btn").on("click", handleButtonClick);
 });
