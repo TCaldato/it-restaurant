a1bc350721a0727e24c9463ec1b4de27718480e1
TCaldato <>
2024-01-28 19:19:47 +0000

    Error on Heroku

--- reservations/views.py
+++ reservations/views.py
@@ -1,19 +1,141 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Booking
+from .forms import ReservationForm
+from datetime import datetime
+from django.db.models import Count, Sum
+from django.db.models.functions import TruncDate
+from django.core.exceptions import ValidationError
+
+# The number of appointments that can be taken
+num_appointments = 1
+
+# View to render upcoming bookings from the current date
+def view_reservation(request):
+    """
+    View Reservation:
+    Renders a view displaying upcoming bookings from the current date.
+    """
+    today_date = datetime.now()
+    bookings = Booking.objects.filter(date__gte=today_date)
+    context = {"bookings": bookings}
+    return render(request, "reservations/views_reservation.html", context)
 
 
-urlpatterns = [
-    path("", views.view_reservation, name="reservations"),
-    path("create_booking/", views.create_booking, name="create_booking"),
-    path("edit_booking/<int:booking_id>/", views.edit_booking, name="edit_booking"),
-    path(
-        "cancel_booking/<int:booking_id>/", views.cancel_booking, name="cancel_booking"
-    ),
-    path(
-        "reservations/edit_booking/<int:booking_id>/",
-        views.edit_booking,
-        name="edit_booking",
-    ),
-]
+# View to handle the creation of a new booking
+@login_required()
+def create_booking(request):
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            chosen_date = form.cleaned_data["date"]
+            chosen_time = form.cleaned_data["start_time"]
+            num_people = form.cleaned_data["num_people"]
+
+            # Check if the user has already booked for the chosen date
+            existing_booking = Booking.objects.filter(
+                user=request.user,
+                date=chosen_date,
+            ).first()
+
+            if existing_booking:
+                messages.error(request, "You have already booked for this date.")
+                return redirect("create_booking")
+
+            # Check if the total number of people booked for the chosen time exceeds the limit
+            total_people_booked = Booking.objects.filter(
+                date=chosen_date, start_time=chosen_time
+            ).aggregate(total_people=Sum("num_people"))["total_people"]
+
+            if total_people_booked is None:
+                total_people_booked = 0
+
+            if total_people_booked + num_people > 15:
+                messages.error(
+                    request,
+                    f"No Booking available for {chosen_date} at {chosen_time}. Please choose another time.",
+                )
+                return redirect("create_booking")
+
+            # Continue with the booking creation if the limit is not exceeded
+            form.instance.user = request.user
+            form.save()
+            messages.success(
+                request,
+                f"Your appointment for {request.user.username} has been confirmed.",
+            )
+            return redirect("reservations")
+    else:
+        form = ReservationForm()
+
+    context = {"form": form}
+    return render(request, "reservations/create_booking.html", context)
 
 
+@login_required()
+def edit_booking(request, booking_id):
+    """
+    Edit booking:
+    Handles the editing of an existing booking for the logged-in user.
+    """
+
+    # Get the existing booking or return a 404 if not found
+    booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+
+    if request.method == "POST":
+        # If the request method is POST, process the form data
+        form = ReservationForm(request.POST, instance=booking)
+        if form.is_valid():
+            # Extract relevant data from the form
+            chosen_date = form.cleaned_data["date"]
+            chosen_time = form.cleaned_data["start_time"]
+            num_people = form.cleaned_data["num_people"]
+
+            # Check if there are conflicting bookings for the same date and time
+            num_same_bookings = (
+                Booking.objects.filter(
+                    date=chosen_date, start_time=chosen_time, user=request.user
+                )
+                .exclude(id=booking_id)
+                .count()
+            )
+
+            if num_same_bookings >= num_appointments:
+                # If there are too many conflicting bookings, display an error message
+                messages.error(
+                    request, "No appointment available or this is your reservation"
+                )
+                return redirect("edit_booking", booking_id)
+            else:
+                # Save the form if there are no conflicts
+                form.save()
+                messages.success(
+                    request,
+                    f"Your appointment for {request.user.username} has been changed.",
+                )
+                return redirect("reservations")
+    else:
+        # If the request method is not POST, initialize the form with existing booking data
+        form = ReservationForm(instance=booking)
+
+    # Prepare the context for rendering the template
+    context = {"form": form}
+    return render(request, "reservations/edit_booking.html", context)
+
+
+@login_required()
+def cancel_booking(request, booking_id):
+    """
+    Cancel reservation:
+    Handles the cancellation of an existing booking for the logged-in user.
+    """
+
+    # Get the existing booking or return a 404 if not found
+    booking = get_object_or_404(Booking, id=booking_id)
+
+    # Delete the booking and display a warning message
+    booking.delete()
+    messages.warning(request, "Your booking has been deleted")
+    return redirect("reservations")
